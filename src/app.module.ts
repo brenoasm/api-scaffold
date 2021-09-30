6ccaa36201d7f42dbb835397421078b56112e6d0
Breno Augusto <breno.augustosm@gmail.com>
2021-09-30 10:53:24 -0300

    Added the login flow

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './commons/database/database.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, AuthModule, UserModule],
   controllers: [],
   providers: [],
 })
---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+import { UserModule } from '../user/user.module';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './auth.controller';
+import { LocalAuthGuard } from './guards/local.auth.guard';
+import { JwtAuthGuard } from './guards/jwt.auth.guard';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get<string>(
+            'JWT_EXPIRATION_IN_SECONDS',
+          )}s`,
+        },
+      }),
+    }),
+  ],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    LocalAuthGuard,
+    JwtAuthGuard,
+  ],
+  exports: [AuthService, LocalAuthGuard, JwtAuthGuard],
+  controllers: [AuthController],
+})
+export class AuthModule {}
